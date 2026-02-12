20bb78669943ffefc49ed07e7ca574b5080deffb
anjalidarapureddy <141325831+anjalidarapureddy@users.noreply.github.com>
2026-02-12 17:38:04 +0530

    Update Jenkins installation method in main.tf

--- terraform-demo/main.tf
+++ terraform-demo/main.tf
@@ -9,12 +9,11 @@ resource "aws_instance" "terra" {
     }
     user_data = <<-EOF
     #!/bin/bash
-    apt update -y
-    apt install -y openjdk-17-jdk
-    curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-    apt update -y
-    apt install -y jenkins
+    sudo apt update -y
+    sudo apt install -y openjdk-17-jdk
+    wget https://pkg.jenkins.io/debian-stable/binary/jenkins_2.452.3_all.deb
+    sudo dpkg -i jenkins_2.452.3_all.deb
+    sudo apt -f install -y
     systemctl enable jenkins
     systemctl start jenkins
     EOF
